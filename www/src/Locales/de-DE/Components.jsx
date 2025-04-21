7fcad464445137494b625105896220c293479cec
Atahan Alp <atahanpla@gmail.com>
2025-04-21 22:58:53 +0300

    init commit

---
+++ www/src/Locales/de-DE/Components.jsx
@@ -0,0 +1,19 @@
+export default {
+	'language-selector': {
+		en: 'English (US)',
+		nl: 'Nederlands (Dutch)',
+		'pt-BR': 'Português Brasil (Brazilian Portuguese)',
+		'zh-CN': '简体中文 (Simplified Chinese)',
+		'de-DE': 'Deutsch (German)',
+		'ja-JP': '日本語 (Japaanische)',
+		'ko-KR': '한국어 (Koreanisch)',
+	},
+	'color-scheme': {
+		dark: 'Dunkel',
+		light: 'Hell',
+		auto: 'Automatisch',
+	},
+	'keyboard-mapper': {
+		'key-header': 'Taste',
+	},
+};
